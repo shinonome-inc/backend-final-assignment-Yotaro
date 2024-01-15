6883ad46d452bc020e1a65cddc4d8c73ba3e71cc
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-01-15 18:37:14 +0900

    ログイン・ログアウト機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login
-from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import SignupForm
@@ -8,7 +8,7 @@ from .forms import SignupForm
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = settings.LOGIN_REDIRECT_URL
 
     def form_valid(self, form):
         response = super().form_valid(form)
