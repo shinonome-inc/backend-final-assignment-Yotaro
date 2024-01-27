5abc2476460e6d3652614573d5045e837a10eea5
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-01-27 14:22:29 +0900

    LOGIN_REDIRECT_URLのインポートを変更

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,7 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-
-from mysite.settings import LOGIN_REDIRECT_URL
 
 from .forms import SignupForm
 
@@ -11,7 +9,7 @@ from .forms import SignupForm
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy(LOGIN_REDIRECT_URL)
+    success_url = settings.LOGIN_REDIRECT_URL
 
     def form_valid(self, form):
         response = super().form_valid(form)
