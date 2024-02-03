abd039d43eb00c5782b7c31569222cc10f064f3c
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-02-03 21:53:55 +0900

    ツイート削除機能

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, TemplateView
+from django.views.generic import CreateView, DeleteView, DetailView, TemplateView
 
 from .models import Tweet
 
@@ -30,3 +30,13 @@ class TweetCreateView(LoginRequiredMixin, CreateView):
 class TweetDetailView(LoginRequiredMixin, DetailView):
     template_name = "tweets/detail.html"
     model = Tweet
+
+
+class TweetDeleteView(UserPassesTestMixin, DeleteView):
+    template_name = "tweets/delete.html"
+    success_url = reverse_lazy("tweets:home")
+    model = Tweet
+
+    def test_func(self):
+        object = self.get_object()
+        return object.user == self.request.user
