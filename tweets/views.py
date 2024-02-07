47e6cc0f51800699ebd6193e0db3eb6baa7c5a19
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-02-07 16:33:15 +0900

    フォーマット修正

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, TemplateView
 
