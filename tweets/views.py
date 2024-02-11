a7f335cfcd3940a90a69969d97eb396fbe2e6888
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-02-12 01:40:41 +0900

    delete.htmlの修正

--- tweets/views.py
+++ tweets/views.py
@@ -29,6 +29,7 @@ class TweetDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["profile_user"] = self.object.user
+        context["tweet"] = self.object
         return context
 
 
