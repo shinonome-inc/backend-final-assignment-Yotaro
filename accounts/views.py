9aa7391beac0bfc9ecd8e9c5297e8f3fea22eaef
YotaroKono <yotaro20021210@ezweb.ne.jp>
2024-02-20 18:57:39 +0900

    フォロー・フォロー解除機能

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponseBadRequest
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView, ListView
 
 from tweets.models import Tweet
 
 from .forms import SignupForm
-from .models import User
+from .models import Connection, User
 
 
 class SignupView(CreateView):
@@ -37,4 +40,34 @@ class UserProfileView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["profile_user"] = self.profile_user
+        context["following"] = Connection.objects.filter(follower__username=self.profile_user).count()
+        context["follower"] = Connection.objects.filter(following__username=self.profile_user).count()
         return context
+
+
+class FollowView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        following_name = self.kwargs["username"]
+        following = get_object_or_404(User, username=following_name)
+
+        if request.user == following:
+            return HttpResponseBadRequest("自分自身のユーザーをフォローすることはできません。")
+        elif Connection.objects.filter(follower=self.request.user, following=following).exists():
+            return HttpResponseBadRequest("既にフォローしています．")
+        else:
+            Connection.objects.create(follower=request.user, following=following)
+            messages.success(request, f"{following.username}をフォローしました")
+            return redirect("tweets:home")
+
+
+class UnFollowView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        following_name = self.kwargs["username"]
+        following = get_object_or_404(User, username=following_name)
+
+        if following == request.user:
+            return HttpResponseBadRequest("自分自身のユーザーに操作することはできません。")
+        else:
+            Connection.objects.filter(follower=request.user, following=following).delete()
+
+        return redirect("tweets:home")
